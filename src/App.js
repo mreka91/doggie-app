4ff698f1a4df46d798494dbb271df02a5a561b57
Reka Madarasz <mreka91@gmail.com>
2021-04-12 20:23:27 +0200

    test

--- src/App.js
+++ src/App.js
@@ -5,23 +5,21 @@ import { Router, Link } from "@reach/router";
 //pages
 import Home from "./pages/Home";
 import Pictures from "./pages/Pictures";
-import NotFound from "./pages/NotFound";
 import Facts from "./pages/Facts";
 
 function App() {
   return (
     <>
       <nav className="nav">
-        <Link to="/">Home</Link>
-        <Link to="pictures">Pictures</Link>
-        <Link to="facts">Facts</Link>
+        <Link to={process.env.PUBLIC_URL + "/"}>Home</Link>
+        <Link to={process.env.PUBLIC_URL + "/pictures"}>Pictures</Link>
+        <Link to={process.env.PUBLIC_URL + "/facts"}>Facts</Link>
       </nav>
       <div className="App">
         <Router>
-          <Home path="/" />
-          <Pictures path="/pictures" />
-          <Facts path="/facts" />
-          <NotFound default />
+          <Home path={process.env.PUBLIC_URL + "/"} />
+          <Pictures path={process.env.PUBLIC_URL + "/pictures"} />
+          <Facts path={process.env.PUBLIC_URL + "/facts"} />
         </Router>
       </div>
     </>
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,4 +1,3 @@
-import React from "react";
 import PropTypes from "prop-types";
 import { TitleStyled } from "./styles.js";
 
