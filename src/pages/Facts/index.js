24bbf0a4f115c903fe91f680062f110cc940478f
Reka Madarasz <mreka91@gmail.com>
2021-04-13 11:21:19 +0200

    fact fetch

--- src/pages/Facts/index.js
+++ src/pages/Facts/index.js
@@ -2,23 +2,24 @@ import React from "react";
 import Button from "../../components/Button";
 import "./index.css";
 import Title from "../../components/Title";
+import Frame from "../../components/Frame";
 
 const Facts = () => {
-  /*   let [facts, setFacts] = React.useState([]);
+  let [facts, setFacts] = React.useState([]);
 
   React.useEffect(() => {
-    fetch("api/v1/resources/dogs?number=1")
+    fetch("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1")
       .then((res) => res.json())
       .then((json) => {
         setFacts(json[0].fact);
         console.log(json);
       });
-  }, []); */
+  }, []);
 
   return (
     <div className="fact-view">
       <Title>Random doggie fact</Title>
-      {/* <Frame factText={facts} /> */}
+      <Frame factText={facts} />
       <Button
         buttonText="Give me another doggie fact"
         handleClick={() => window.location.reload(false)}
