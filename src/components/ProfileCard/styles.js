3318bb1cfe76a9c164b4b74409272f76f9371e41
Reka Madarasz <mreka91@gmail.com>
2021-04-19 16:15:09 +0200

    small fixes

--- src/components/ProfileCard/styles.js
+++ src/components/ProfileCard/styles.js
@@ -1,32 +1,34 @@
-
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ProfileStyled = styled.article`
-background: rgba(252, 92, 92, 0.795);
-background: linear-gradient(90deg, rgba(252, 92, 92, 0.733) 0%, rgba(252, 60, 130, 0.774) 100%);
-text-align: center;
-font-family: GothamRounded-Light;
-max-width:400px;
-margin-bottom: 10px;
+  background: rgba(252, 92, 92, 0.9);
+  background: linear-gradient(
+    90deg,
+    rgba(252, 92, 92, 0.9) 0%,
+    rgba(252, 60, 130, 0.9) 100%
+  );
+  text-align: center;
+  font-family: GothamRounded-Light;
+  max-width: 400px;
+  margin-bottom: 10px;
+  margin-top: 30px;
 `;
 
 export const ProfileImageStyled = styled.img`
-border-radius: 50%;
-width: 200px;
-margin: 15px;
+  border-radius: 50%;
+  width: 200px;
+  margin: 15px;
 `;
 
 export const ProfileTextStyled = styled.div`
-width:350px;
+  width: 350px;
 `;
 
 export const ProfileTitleStyled = styled.h3`
-font-size:22px;
+  font-size: 22px;
 `;
 
 export const ProfileParStyled = styled.p`
-font-size: 18px;
-padding:15px;
+  font-size: 18px;
+  padding: 15px;
 `;
-
-
--- src/pages/Barks/index.js
+++ src/pages/Barks/index.js
@@ -3,15 +3,23 @@ import Messages from "../../components/Messages";
 import Title from "../../components/Title";
 import "./index.css";
 
-
 const Barks = () => {
   return (
     <div className="matched">
       <Title>See all your barks!&#128054;</Title>
       <Messages headline="Elsie" message="hej snygging, ses ikväll?" />
-      <Messages headline="Fido" message="va det du som kissade på segelvägen nyss?" />
-      <Messages headline="Otis" message="får jag bjuda dig på tuggben ikväll?" />
-      <Messages headline="Trixa" message="hej hur mår du? matte drog just fram dammsugaren :(" />
+      <Messages
+        headline="Fido"
+        message="va det du som kissade på segelvägen nyss?"
+      />
+      <Messages
+        headline="Otis"
+        message="får jag bjuda dig på tuggben ikväll?"
+      />
+      <Messages
+        headline="Trixa"
+        message="hej hur mår du? matte drog just fram dammsugaren :("
+      />
     </div>
   );
 };
